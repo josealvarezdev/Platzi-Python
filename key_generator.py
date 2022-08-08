0ba9e815edc17c6e98172f8d610f5a341fa1e381
Bester <besster.gg@gmail.com>
2022-08-08 02:06:34 -0300

    Key Generator

---
+++ key_generator.py
@@ -0,0 +1,26 @@
+import random
+
+def gen_passwords():
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    characters = MAYUS + MINUS + NUMS + CHARS
+    password = []
+
+    for i in range (20):
+        random_char = random.choice(characters)
+        password.append(random_char)
+
+    password = "".join(password)
+    return password
+
+
+def run():
+    password = gen_passwords()
+    print('Your new password is: ' + password)
+
+
+if __name__ == '__main__':
+    run()
